f46cd18283ed9224e7a5264475e2570ec7a4c785
adasq <asnaebaem@gmail.com>
2015-06-30 21:37:21 +0200

    based on heroku example

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
+var express = require('express');
+var app = express();
 
-var express =require('express');
+app.set('port', (process.env.PORT || 5000));
 
- 
-
-
-var app = express(); 
-
-	app.set('port',  80);
-	app.use(express.static(__dirname + '/static'));
-
-var server = require('http').createServer(app);
-server.listen(80);
+app.use(express.static(__dirname + '/static'));
 
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
-
-
-
-
-
-
-// var server = http.createServer(function(req, res){
-// 	dropbox.getFile('Tiesto - Club Life 428 (Eelke Kleijn Guestmix) - 20.06.2015.mp3')
-// 	.pipe(res);     
-// });
-// http.get("http://www.google.com/index.html", function(res) {
-//   console.log("Got response: " + res.statusCode);
-// }).on('error', function(e) {
-//   console.log("Got error: " + e.message);
-// });
-
-// server.listen(9999);
