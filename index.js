ff863d999d54fc34bd5909bdb7d4eb99d8117827
Adam Płócieniak <adasq3@gmail.com>
2017-04-16 11:00:45 +0200

    code ref

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-var nconf = require('nconf');
-
+const nconf = require('nconf');
 nconf
 .env()
 .file('./config.json');
 
+const express = require('express');
+const q = require('q');
+const async = require('async');
+const mongoose = require('mongoose');
+const _ = require('underscore');
+const Playlist = require('./modules/Playlist.js');
+const DropboxManager = require('./modules/DropboxManager.js');
 
-var express = require('express');
-var q = require('q');
-var async = require('async');
-var mongoose = require('mongoose');
+const DB_URI = nconf.get('DB_URI');
+const PORT = nconf.get('PORT');
 
-var _ = require('underscore');
-
-var Playlist = require('./modules/Playlist.js');
-
-var DropboxManager = require('./modules/DropboxManager.js');
-
-
-mongoose.connect(nconf.get('DB_URI'));
-var Track = mongoose.model('Track', {
+const Track = mongoose.model('Track', {
 	name: String,
   rev: String
 });
-mongoose.connection.on("open", init);
 
-//------------------------------------------------------------------------------------
-function init(){
-  Track.find().then(function(savedTracks){ 
-    Playlist.setTracks(_.map(savedTracks, function(track){
-      console.log(track);
-      return {
-        name: track.name,
-        rev: track.rev,
-        id: ''+track._id
-      };
-    }));
-    startServer();
-  })
-  // retriveAndSaveTracks(function(tracks){
-  //     startServer();
-  // });
-}
-//------------------------------------------------------------------------------------
+init();
 
+//================================================
 
-
-function startServer(){
-  var app = express();
-
-  app.set('port', (nconf.get('PORT') || 3000));
-  app.use(express.static(__dirname + '/static'));
-
-
-  app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+function init() {
+  async.series([connectDatabase, loadTracks, startServer], (err) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('APP STARTUP SUCCESS');
   });
+}
+
+function loadTracks(cb) {
+  console.log('loading tracks...');
+  Track.find().then(tracks => {
+    console.log(tracks.length);
+    Playlist.setTracks(parseTracks(tracks));
+    cb();
+  });
+}
+
+function connectDatabase(cb){
+  console.log('connectiong database...');
+  mongoose.connect(DB_URI);
+  mongoose.connection.on('open', cb);
+}
+
+function parseTracks(tracks) {
+  return _.map(tracks, ({name, rev, _id}) => {
+      return { name, rev, id: _id.toString() };
+  });
+}
+
+function startServer(cb){
+  console.log('starting server...');
+  const app = express();
+  app.set('port', (PORT || 3000));
+  app.use(express.static(__dirname + '/static'));
+  app.listen(app.get('port'), cb);
 
   var routes = require('./routes/routes.js');
   _.each(routes, function(routePackagePath){
@@ -70,31 +74,15 @@ function startServer(){
   });
 }
 
-//----------------------------------------------------------------------
-
-function getSavedTracks(cb){
- Track.find(function(err, tracks){
-   cb(_.map(tracks, function(track){
-      return {
-        name: track.name,
-        id: track._id+''
-      };
-   })); 
-}); 
-}
-
-
 function retriveAndSaveTracks(cb){
   Track.collection.remove();
-  getDropboxTracks().then(function(tracks){
-    saveTrackNames(tracks).then(function(savedTracks){
+  getDropboxTracks().then(tracks => {
+    saveTrackNames(tracks).then(savedTracks => {
        Playlist.setTracks(savedTracks);
-       console.log(savedTracks);
        cb && cb(savedTracks);
     });
   });
 }
-
 
 function getDropboxTracks(){
 	return DropboxManager.getFiles('/nuta');
@@ -102,7 +90,6 @@ function getDropboxTracks(){
 
 function saveTrackNames(tracks){
   var deferred = q.defer();
-  console.log('saving...');
   var promises =_.map(tracks, function(trackObj){    
     return function(callback){
       var track = new Track({
@@ -110,7 +97,6 @@ function saveTrackNames(tracks){
         rev: trackObj.rev
       });
       track.save(function(err, result){
-
         callback(err, err || {
           rev: result.rev,
           name: result.name,
@@ -129,4 +115,3 @@ function saveTrackNames(tracks){
   });
   return deferred.promise;
 }
-
